223347c82955c88b11b72cbfb0f7bd9a6ceb01a0
Kabir Jain <kabirjain15@gmail.com>
2021-12-04 03:37:10 +0530

    Changes made

--- lib/Screens/seekerHome.dart
+++ lib/Screens/seekerHome.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_assignment/horizontal_card.dart';
+import 'package:flutter_assignment/vertical_card.dart';
 
 final Color _primaryTextColor = const Color(0xFF1A2E35);
 
@@ -96,7 +98,7 @@ class _SeekerHomeState extends State<SeekerHome> {
                         ],
                       ),
                       const SizedBox(
-                        height: 20.0,
+                        height: 10.0,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -118,28 +120,34 @@ class _SeekerHomeState extends State<SeekerHome> {
                                 )),
                           ])),
                           Padding(
-                            padding: const EdgeInsets.only(right: 20.0),
-                            child: Text("Show all",
-                                style: TextStyle(
-                                    fontSize: 17,
-                                    color: _primaryTextColor,
-                                    fontFamily: 'Aeonik-Light')),
-                          )
+                              padding: const EdgeInsets.only(right: 20.0),
+                              child: TextButton(
+                                onPressed: () {},
+                                child: Text("Show all",
+                                    style: TextStyle(
+                                        fontSize: 17,
+                                        color: _primaryTextColor,
+                                        fontFamily: 'Aeonik-Light',
+                                        fontWeight: FontWeight.w300)),
+                              ))
                         ],
                       ),
                       const SizedBox(
-                        height: 20.0,
+                        height: 10.0,
                       ),
                       Container(
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(color: Colors.red[50]),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[100],
+                            borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(15.0),
+                                bottomLeft: Radius.circular(15.0))),
                         child: SizedBox(
                             height: 210,
                             child: ListView.builder(
                                 scrollDirection: Axis.horizontal,
                                 itemCount: 5,
                                 itemBuilder: (context, index) {
-                                  return const JobCard();
+                                  return const HorizontalJobCard();
                                 })),
                       ),
                       const SizedBox(
@@ -167,18 +175,18 @@ class _SeekerHomeState extends State<SeekerHome> {
                       Padding(
                         padding: const EdgeInsets.only(right: 20.0),
                         child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 200,
                           decoration: BoxDecoration(
-                            color: Colors.red[50],
-                          ),
+                              color: Colors.red[50],
+                              borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(15.0),
+                                  topRight: Radius.circular(15.0))),
                           child: SizedBox(
                             height: MediaQuery.of(context).size.height,
                             child: ListView.builder(
-                                shrinkWrap: true,
+                                scrollDirection: Axis.vertical,
                                 itemCount: 5,
                                 itemBuilder: (context, index) {
-                                  return const JobCard();
+                                  return const VerticalJobCard();
                                 }),
                           ),
                         ),
@@ -186,130 +194,5 @@ class _SeekerHomeState extends State<SeekerHome> {
                     ],
                   )),
             )));
-  }
-}
-
-class JobCard extends StatelessWidget {
-  const JobCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Card(
-          color: Colors.white,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30))),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 20.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text("Interior Carpet Designer",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: _primaryTextColor,
-                      fontWeight: FontWeight.bold)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    width: 65,
-                    height: 22,
-                    decoration: BoxDecoration(
-                        color: Colors.grey[50],
-                        border:
-                            Border.all(width: 1.0, color: _primaryTextColor),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(8.0))),
-                    child: Padding(
-                      padding: const EdgeInsets.all(5.0),
-                      child: Center(
-                        child: Text(
-                          "Full Time",
-                          style:
-                              TextStyle(fontSize: 10, color: _primaryTextColor),
-                        ),
-                      ),
-                    ),
-                  ),
-                  RichText(
-                    // ignore: prefer_const_literals_to_create_immutables
-                    text: TextSpan(children: [
-                      TextSpan(
-                          text: "Salary ",
-                          style: TextStyle(
-                              fontSize: 13,
-                              color: _primaryTextColor,
-                              fontWeight: FontWeight.bold)),
-                      TextSpan(
-                          text: "upto ",
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: _primaryTextColor,
-                            fontFamily: 'Aeonik-Light',
-                          )),
-                      TextSpan(
-                          text: "\u20B915000/month",
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: _primaryTextColor,
-                          )),
-                    ]),
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.bookmark,
-                        color: Color(0xFFB4D9FE),
-                        size: 35,
-                      ))
-                ],
-              ),
-              const Text("2+ Years Experience"),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10.0, bottom: 10.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      RichText(
-                          // ignore: prefer_const_literals_to_create_immutables
-                          text: TextSpan(
-                        children: [
-                          TextSpan(
-                              text: "Badonia & Sons\n",
-                              style: TextStyle(
-                                  fontSize: 17,
-                                  color: _primaryTextColor,
-                                  fontWeight: FontWeight.bold)),
-                          TextSpan(
-                              text: "Civil lines, Sagar",
-                              style: TextStyle(
-                                fontSize: 13,
-                                color: _primaryTextColor,
-                                fontFamily: 'Aeonik-Light',
-                              )),
-                        ],
-                      )),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: const Text(
-                          "Apply Now",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8.0)))),
-                      )
-                    ]),
-              ),
-            ]),
-          ),
-        ));
   }
 }
---
+++ lib/horizontal_card.dart
@@ -0,0 +1,135 @@
+// ignore_for_file: prefer_const_declarations
+
+import 'package:flutter/material.dart';
+
+final Color _primaryTextColor = const Color(0xFF1A2E35);
+
+class HorizontalJobCard extends StatelessWidget {
+  const HorizontalJobCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          color: Colors.white,
+          shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0, left: 20.0),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text("Interior Carpet Designer",
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: _primaryTextColor,
+                      fontWeight: FontWeight.bold)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 65,
+                    height: 22,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[50],
+                        border:
+                            Border.all(width: 1.0, color: _primaryTextColor),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Center(
+                        child: Text(
+                          "Full Time",
+                          style:
+                              TextStyle(fontSize: 10, color: _primaryTextColor),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  RichText(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "Salary ",
+                          style: TextStyle(
+                              fontSize: 13,
+                              color: _primaryTextColor,
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: "upto ",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: _primaryTextColor,
+                            fontFamily: 'Aeonik-Light',
+                          )),
+                      TextSpan(
+                          text: "\u20B915000/month",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: _primaryTextColor,
+                          )),
+                    ]),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.bookmark,
+                        color: Color(0xFFB4D9FE),
+                        size: 35,
+                      ))
+                ],
+              ),
+              const Text("2+ Years Experience"),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  right: 10.0,
+                ),
+                child: Row(children: [
+                  RichText(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "Badonia & Sons\n",
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: _primaryTextColor,
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(
+                          text: "Civil lines, Sagar",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: _primaryTextColor,
+                            fontFamily: 'Aeonik-Light',
+                          )),
+                    ],
+                  )),
+                  const SizedBox(
+                    width: 60.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Apply Now",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0)))),
+                  )
+                ]),
+              ),
+            ]),
+          ),
+        ));
+  }
+}
